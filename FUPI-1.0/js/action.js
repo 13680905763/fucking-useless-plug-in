be94768314aa8a0c1d47d24f9e13be450f50d986
177748749@qq.com <9583941+z-zhang-yuhua@user.noreply.gitee.com>
2023-01-14 17:51:42 +0800

    feat 1.0网页背影更换end

--- FUPI-1.0/js/action.js
+++ FUPI-1.0/js/action.js
@@ -1,21 +1,144 @@
-const settingBtn = document.getElementById('settingBtn')
 const feedbackBtn = document.getElementById('feedbackBtn')
-const colorList = document.querySelectorAll('.colorItem')
-colorList.forEach((item) => {
+
+const colorBlock = [
+  'aliceblue',
+  'beige',
+  'antiquewhite',
+  'bisque',
+  'pink',
+  'powderblue',
+  'aquamarine',
+  'mediumpurple'
+]
+
+let resultColor = '' // 选中的颜色
+//监听content发过来的默认背景色
+// chrome.runtime.onMessage.addListener(function (request, sender, callback) {
+//   // 设置默认背景色
+//   console.log(request);
+//   resultColor = request.defaultBgColor || ''
+//   setColorShow(resultColor)
+// })
+
+// 颜色块
+const colorBlockItemElm = document.querySelector('.colorBlock').querySelectorAll('li')
+
+// 提交按钮
+const submitElm = document.querySelector('.submit')
+
+// 颜色展示区域
+const colorShowElm = document.querySelector('.colorShow')
+
+// 颜色块初始化
+colorBlockItemElm.forEach((item, index) => {
+  item.style.background = colorBlock[index]
+  // 监听颜色块点击
   item.addEventListener('click', (e) => {
-    console.log(e.target.dataset.color)
-    // 与content_script通信
+    resultColor = colorBlock[index]
+    setColorShow(resultColor)
   })
 })
 
-// 设置
-// settingBtn.addEventListener('click', () => {
-//   window.open(chrome.extension.getURL('../background/dist/index.html'))
-// })
+// action body背景色初始化
+// const linearGradientColor = colorBlock.join()
+// document.querySelector('body').style.background = `linear-gradient(${linearGradientColor})`
+
+/* 滑块部分 start 直接扒别人的有空研究*/
+let H = document.querySelector('.colorBar')
+let HRect = H.querySelector('rect')
+let HSlide = H.querySelector('.colorSlider')
+let HFlag = false
+
+let Hval = 0
+let Sval = 100
+let Vval = 100
+
+HRect.addEventListener('mousedown', () => {
+  HFlag = true
+})
+HRect.addEventListener('mousemove', (ev) => {
+  if (!HFlag) return
+
+  // ev.offsetY这个值为鼠标相对于源元素的Y坐标，算出滑块在元素中的定位比例
+  let offsetY = ev.offsetY / H.offsetHeight
+
+  HSlide.style.top = ev.offsetY - 8 + 'px'
+
+  // 因为H值的范围是0~360，乘以比例就可以得出一个颜色值了
+  Hval = 360 * offsetY
+
+  resultColor = `rgb(${hsvtorgb(Hval, Sval, Vval).join(',')})`
+  setColorShow(resultColor)
+})
+HRect.addEventListener('mouseup', () => {
+  HFlag = false
+})
+
+function hsvtorgb(h, s, v) {
+  s = s / 100
+  v = v / 100
+  var h1 = Math.floor(h / 60) % 6
+  var f = h / 60 - h1
+  var p = v * (1 - s)
+  var q = v * (1 - f * s)
+  var t = v * (1 - (1 - f) * s)
+  var r, g, b
+  switch (h1) {
+    case 0:
+      r = v
+      g = t
+      b = p
+      break
+    case 1:
+      r = q
+      g = v
+      b = p
+      break
+    case 2:
+      r = p
+      g = v
+      b = t
+      break
+    case 3:
+      r = p
+      g = q
+      b = v
+      break
+    case 4:
+      r = t
+      g = p
+      b = v
+      break
+    case 5:
+      r = v
+      g = p
+      b = q
+      break
+  }
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)]
+}
+/* 滑块部分 end */
+
+// 设置选中颜色
+const setColorShow = (color) => {
+  colorShowElm.style.background = color
+}
+// 提交点击
+submitElm.addEventListener('click', () => {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true
+    },
+    (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { bgColor: resultColor })
+    }
+  )
+})
 
 // 反馈
-// feedbackBtn.addEventListener('click', () => {
-//   chrome.tabs.create({
-//     url: 'https://github.com/13680905763/fucking-useless-plug-in/issues'
-//   })
-// })
+feedbackBtn.addEventListener('click', () => {
+  chrome.tabs.create({
+    url: 'https://github.com/13680905763/fucking-useless-plug-in/issues'
+  })
+})
--- FUPI-1.0/js/content_script.js
+++ FUPI-1.0/js/content_script.js
@@ -1,24 +1,18 @@
-// 往掘金插入运行js按钮
-// const jsElm = document.querySelectorAll('.language-javascript')
-// console.log(jsElm);
-// const createRunElm = () => {
-//   const runElm = document.createElement('span')
-//   runElm.innerText = '运行代码'
-//   runElm.className = 'copy-code-btn'
-//   runElm.style = 'right: 150px'
-//   runElm.addEventListener('click',(e)=>{
-//     console.log(e);
-//     // e.preventDefault()
-//     // alert('打开控制台')
-//   })
-//   return runElm
-// }
-
-// jsElm.forEach((item) => {
-//   item.appendChild(createRunElm())
-//   console.log(item);
-// })
-console.log('注入成功');
-
+console.log('注入成功')
 const bodyElm = document.querySelector('body')
-bodyElm.style = 'background:antiquewhite'
+const defaultBgColor = localStorage.getItem('bgColor') || ''
+// chrome.runtime.sendMessage({
+//     defaultBgColor
+// }).catch()
+const setBgColor = (color) => {
+  localStorage.setItem('bgColor',color)
+  bodyElm.style.background = color
+  console.log('setBgColor');
+}
+setBgColor(defaultBgColor)
+
+// 监听action发送过来的颜色
+chrome.runtime.onMessage.addListener(function (request, sender, callback) {
+  setBgColor(request.bgColor)
+  console.log('get popup color',request);
+})
